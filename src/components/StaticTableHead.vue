63b6328ca146b3d98d107c0c2b580afd3e2fb753
xsswsx <xgpssyh0903@sina.com>
2023-07-03 23:48:22 +0800

    style(frontend): deleted redundant code

--- src/components/StaticTableHead.vue
+++ src/components/StaticTableHead.vue
@@ -1,15 +1,5 @@
 <script setup lang="ts">
 
-var conConfigs = [
-  {prop: 'id', label: '车次号'},
-  {prop: 'name', label: '列车名'},
-  {prop: 'duration', label: '历时'},
-  {prop: 'start_station_id', label: '出发站'},
-  {prop: 'end_station_id', label: '到达站'},
-  {prop: 'departure_time', label: '出发时间'},
-  {prop: 'arrival_time', label: '到达时间'},
-  {prop: 'ticket_info', label: '余票'},
-]
 
 </script>
 
