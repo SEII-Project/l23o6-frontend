b99b21f0982b427ae260a0e9198ace68a53bd64f
irisalt <2502391649@qq.com>
2023-07-04 22:52:44 +0800

    refactor(frontend): delete order from menu and show it only when submitted an order

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -60,7 +60,7 @@ async function change() {
     <!--    <el-menu-item index="/route">路线管理</el-menu-item>-->
     <!--    <el-menu-item index="/train">车次管理</el-menu-item>-->
 <!--    -->
-    <el-menu-item index="/order">订单</el-menu-item>
+<!--    <el-menu-item index="/order">订单</el-menu-item>-->
     <el-menu-item h="full" @click="change()">
       <button class="border-none w-full bg-transparent cursor-pointer" style="height: var(--el-menu-item-height)">
         <i inline-flex i="dark:ep-moon ep-sunny" />
