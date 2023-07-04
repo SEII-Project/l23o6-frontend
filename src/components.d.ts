5298cca45e9dd2939761083916ef1e5a73f30d20
cardigan1008 <211250058@smail.nju.edu.cn>
2023-07-04 21:46:04 +0800

    fix: price in request

--- src/components.d.ts
+++ src/components.d.ts
@@ -23,6 +23,7 @@ declare module '@vue/runtime-core' {
     ElDescriptionsItem: typeof import('element-plus/es')['ElDescriptionsItem']
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElDrawer: typeof import('element-plus/es')['ElDrawer']
+    ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElHeader: typeof import('element-plus/es')['ElHeader']
